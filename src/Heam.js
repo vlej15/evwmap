106d71887bfe5418dc779f1247df9453187cc669
SiniRani <kul0107@g.yju.ac.kr>
2021-05-04 18:20:14 +0900

    Header_21_05_04

--- src/Heam.js
+++ src/Heam.js
@@ -2,84 +2,88 @@ import React from "react";
 import { Link, Route, Switch } from "react-router-dom";
 
 function Heam() {
-  function logout() {
-    localStorage.removeItem("id");
-    window.location.replace("/");
-  }
-  return (
-    <div className="pcmenu">
-      <div className="global_box">
-        <ul className="global_join_box">
-          <li className="global_list_point">
-            {localStorage.getItem("id") == null ? null : (
-              <a className="point_box" href="#">
-                MY POINT : {localStorage.getItem("user_point")}P
-              </a>
-            )}
-          </li>
-          <li className="global_list login">
-            {localStorage.getItem("id") == null ? (
-              <Link to="/login">
-                <a className="login_box" href="#">
-                  LOGIN
-                </a>
-              </Link>
-            ) : (
-              <Link to="/">
-                <a className="login_box" href="#" onClick={logout}>
-                  LOGOUT
-                </a>
-              </Link>
-            )}
-          </li>
-          <li className="global_list">
-            {localStorage.getItem("id") == null ? (
-              <Link to="/signup">
-                <a className="join_box" href="#">
-                  JOIN
-                </a>
-              </Link>
-            ) : (
-              <Link to="/activity">
-                <a className="join_box" href="#">
-                  MY PAGE
-                </a>
-              </Link>
-            )}
-          </li>
-        </ul>
-      </div>
-      <div className="global_menu">
-        <nav className="gub-wrap">
-          <ul className="mainmenu_ul">
-            <li className="gub_02 main_li">
-              <Link to="/map">
-                <a className="map_a main_menu_list" href="">
-                  <h3 className="main_bar">LOAD MAP</h3>
-                </a>
-              </Link>
-            </li>
+    function logout() {
+        localStorage.removeItem("id");
+        window.location.replace("/");
+    }
+    return (
+        <div className="pcmenu">
+            <div className="global_box">
+                <ul className="global_join_box">
+                    <li className="global_list_point">
+                        {localStorage.getItem("id") == null ? null : (
+                            <a className="point_box" href="#">
+                                MY POINT : {localStorage.getItem("user_point")}P
+                            </a>
+                        )}
+                    </li>
+                    <li className="global_list login">
+                        {localStorage.getItem("id") == null ? (
+                            <Link to="/login">
+                                <a className="login_box" href="#">
+                                    LOGIN
+                                </a>
+                            </Link>
+                        ) : (
+                            <Link to="/">
+                                <a
+                                    className="login_box"
+                                    href="#"
+                                    onClick={logout}
+                                >
+                                    LOGOUT
+                                </a>
+                            </Link>
+                        )}
+                    </li>
+                    <li className="global_list">
+                        {localStorage.getItem("id") == null ? (
+                            <Link to="/signup">
+                                <a className="join_box" href="#">
+                                    JOIN
+                                </a>
+                            </Link>
+                        ) : (
+                            <Link to="/activity">
+                                <a className="join_box" href="#">
+                                    MY PAGE
+                                </a>
+                            </Link>
+                        )}
+                    </li>
+                </ul>
+            </div>
+            <div className="global_menu">
+                <nav className="gub-wrap">
+                    <ul className="mainmenu_ul">
+                        <li className="gub_02 main_li">
+                            <Link to="/map">
+                                <a className="map_a main_menu_list" href="">
+                                    <h3 className="main_bar">MAP</h3>
+                                </a>
+                            </Link>
+                        </li>
 
-            <li className="gub_03 main_li">
-              <Link to="/notice">
-                <a className="com_a main_menu_list" href="">
-                  <h3 className="main_bar">COMMUNITY</h3>
-                </a>
-              </Link>
-            </li>
+                        <li className="gub_03 main_li">
+                            <Link to="/notice">
+                                <a className="com_a main_menu_list" href="">
+                                    <h3 className="main_bar">COMMUNITY</h3>
+                                </a>
+                            </Link>
+                        </li>
 
-            <li className="gub_04 main_li">
-              <Link to="/faq">
-                <a className="con_a main_menu_list" href="">
-                  <h3 className="main_bar">CONTACT</h3>
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+                        <li className="gub_04 main_li">
+                            <Link to="/faq">
+                                <a className="con_a main_menu_list" href="">
+                                    <h3 className="main_bar">CONTACT</h3>
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
 }
 
 export default Heam;
